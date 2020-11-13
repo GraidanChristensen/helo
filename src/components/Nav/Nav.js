0dec8227772fe3b27526443fd0b77912cb5177c4
GraidanChristensen <graidanc@gmail.com>
2020-11-13 16:07:04 -0700

    redux set up doesn't update yet

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+
 class Nav extends Component{
     render(){
             return(
                 <div>
+                    <h5>{`Username: ${this.props.username}`}</h5>
                     <button><Link to="/dashboard">Home</Link></button>
                     <button><Link to="/new">New Post</Link></button>
                     <button><Link to="/">Logout</Link></button>
+                    {console.log(this.props)}
                 </div>
             )
         }
     
 }
-
-export default Nav;
+function mapStateToProps(state){
+    return{
+        username: state.username,
+        picture: state.picture
+    }
+}
+export default connect(mapStateToProps)(Nav);
---
+++ src/redux/reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    id: null,
+    username: '',
+    picture: ''
+}
+
+export default function(state = initialState, action){
+    switch(action.type){
+        default: return state;
+    }
+}
